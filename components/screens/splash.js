73e29dde76fb7876ff32c9358efbdaf478f3a2d6
unknown <owaisraza54@yahoo.com>
2018-11-24 03:36:02 -0800

    splash screen almost done

--- components/screens/splash.js
+++ components/screens/splash.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View ,Image} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -13,9 +13,14 @@ export default class Splash extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>BIENVENUE</Text> 
+         <Image
+          style = {{ width: 200, height: 200 }}
+          source={require('../img/gs.png')}
+        />
+        <Text style={styles.welcome}>GEO-SECURITE</Text>
+        
+       
       </View>
     );
   }
@@ -29,13 +34,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    fontWeight:'bold'
   },
 });
